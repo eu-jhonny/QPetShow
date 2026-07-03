d3369a04cce8c6030978391d908c2a2ed8f1d6ed
eu-jhonny <srtutty123@gmail.com>
2026-07-03 19:55:52 -0300

    Update resend.ts

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,7 +1,15 @@
 import { Resend } from "resend";
 
-if (!process.env.EMAIL_API_KEY) {
-  throw new Error("EMAIL_API_KEY não configurada.");
-}
+let resend: Resend | null = null;
 
-export const resend = new Resend(process.env.EMAIL_API_KEY);
+export function Resend() {
+  if (!process.env.EMAIL_API_KEY) {
+    throw new Error("EMAIL_API_KEY não configurada.");
+  }
+
+  if (!resend) {
+    resend = new Resend(process.env.EMAIL_API_KEY);
+  }
+
+  return resend;
+}
